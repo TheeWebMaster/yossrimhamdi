ba5cb5dde872fbe0cb7f08c8f18b244144ba56ff
youssri mhamdi <bavary1515@gmail.com>
2020-03-25 20:02:30 +0100

    clearing watch.js gulp file.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,8 +3,7 @@ import { createServer, reloadBrowser, injectCss } from './server';
 import compileCss from './styles';
 
 const eye = () => {
-  createServer();
   watch('./app/index.html', reloadBrowser);
   watch('./app/assets/styles/**/*.css', series(compileCss, injectCss));
 };
-task('watch', eye);
+task('watch', series(createServer, eye));
