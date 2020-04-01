e61ad56fbf9e0fe1c34b002b3eec27ad6a25d6ad
youssri mhamdi <bavary1515@gmail.com>
2020-04-01 17:23:25 +0100

    fixed mile stone callback issue.

--- app/assets/scripts/modules/AnimateMileStonesOnScroll.js
+++ app/assets/scripts/modules/AnimateMileStonesOnScroll.js
@@ -3,32 +3,19 @@ import SetupParallaxEnviroment from './SetupParallaxEnviroment';
 
 class AnimateMileStonesOnScroll extends SetupParallaxEnviroment {
   constructor(scroller) {
-    super(scroller, '.milestone', 200 - window.innerHeight);
+    function displayMileStoneDiscription(element) {
+      element.querySelector('.milestone__description').classList.add('milestone__description--is-visible');
+    }
+    super(scroller, '.milestone', 200 - window.innerHeight, displayMileStoneDiscription);
     this.scroller = scroller;
     this.xLimits = { start: -50, end: 0 };
     this.setTranslateXObjects();
   }
-  /*   getrelative() {
-    const relative = [];
-    this.elements.forEach((element, i) => {
-      relative.push(
-        new ScrollInput(
-          this.scroller,
-          this.topBoundings[i],
-          this.displayMileStoneDiscription.bind(this, element)
-        )
-      );
-    });
-    return relative;
-  } */
   setTranslateXObjects() {
     this.elements.forEach((element, i) => {
       const topBorderElement = element.querySelector('.milestone__top-border');
       new TranslateX(this.scroller, this.relativeScrollInputs[i], topBorderElement, this.xLimits, '%');
     });
-  }
-  displayMileStoneDiscription(element) {
-    element.querySelector('.milestone__description').classList.add('milestone__description--is-visible');
   }
 }
 
