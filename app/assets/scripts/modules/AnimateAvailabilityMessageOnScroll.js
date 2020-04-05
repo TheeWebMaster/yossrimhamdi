dfe8086e374251f6650e5d268b53143679133da1
youssri mhamdi <bavary1515@gmail.com>
2020-04-05 21:36:53 +0100

    fixed availability message shaking on page load.

--- app/assets/scripts/modules/AnimateAvailabilityMessageOnScroll.js
+++ app/assets/scripts/modules/AnimateAvailabilityMessageOnScroll.js
@@ -3,7 +3,7 @@ class AnimateAvailabilityMessageOnScroll {
     this.scroller = scroller;
     this.message = {
       container: document.querySelector('.header__availability-message ul'),
-      items: document.querySelectorAll('.header__availability-message li')
+      items: document.querySelectorAll('.header__availability-message li'),
     };
     this.range = [0.4, 0.3, 0.2, 0.1];
     scroller.onScroll(this.animateMessage.bind(this));
@@ -13,7 +13,7 @@ class AnimateAvailabilityMessageOnScroll {
     this.addSpaceBetweenMessageLIs(yPosition);
   }
   moveMessageListUp(yPosition) {
-    this.message.container.style.transform = `translateY(-${yPosition + 20}px)`;
+    this.message.container.style.transform = `translateY(-${yPosition * 1.5}px)`;
   }
   addSpaceBetweenMessageLIs(yPosition) {
     this.message.items.forEach((item, i) => {
