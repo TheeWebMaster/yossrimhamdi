66d00317fe6f6450468222d1af6b8d7abfc3c158
youssri mhamdi <bavary1515@gmail.com>
2020-04-12 14:53:26 +0100

    fixed availability message animation of mobile.

--- app/assets/scripts/modules/AnimateAvailabilityMessageOnScroll.js
+++ app/assets/scripts/modules/AnimateAvailabilityMessageOnScroll.js
@@ -5,15 +5,22 @@ class AnimateAvailabilityMessageOnScroll {
       container: document.querySelector('.header__availability-message ul'),
       items: document.querySelectorAll('.header__availability-message li'),
     };
-    this.range = [0.4, 0.3, 0.2, 0.1];
+    this.range = this.getRange();
+    this.moveUpOffset = this.getMoveUpOffset();
     scroller.addListener(this.animateMessage.bind(this));
+  }
+  getMoveUpOffset() {
+    return window.innerWidth >= 1024 ? 1.5 : 1.2;
+  }
+  getRange() {
+    return window.innerWidth >= 1024 ? [0.4, 0.3, 0.2, 0.1] : [0.8, 0.6, 0.4, 0.2];
   }
   animateMessage({ offset: { y } }) {
     this.moveMessageListUp(y);
     this.addSpaceBetweenMessageLIs(y);
   }
   moveMessageListUp(yPosition) {
-    this.message.container.style.transform = `translateY(-${yPosition * 1.5}px)`;
+    this.message.container.style.transform = `translateY(-${yPosition * this.moveUpOffset}px)`;
   }
   addSpaceBetweenMessageLIs(yPosition) {
     this.message.items.forEach((item, i) => {
