837281a86ab19ef9dc0560ff5929208c840e8185
youssri mhamdi <bavary1515@gmail.com>
2020-04-18 22:21:41 +0100

    refactored SetupParallaxEnviroment class.

--- app/assets/scripts/modules/SetupParallaxEnviroment.js
+++ app/assets/scripts/modules/SetupParallaxEnviroment.js
@@ -1,38 +1,24 @@
 import ScrollInput from './ScrollInput';
 
 class SetupParallaxEnviroment {
-  constructor(scroller, className, endBoundingOffset = 0, callback = function () {}) {
-    this.callback = callback;
-    this.scroller = scroller;
+  constructor(className, endBoundingOffset = 0) {
     this.elements = document.querySelectorAll(className);
     this.endBoundingOffset = endBoundingOffset;
     this.topBoundings = this.getTopElementsBounding();
     this.relativeScrollInputs = this.getRelativeScrollInputs();
   }
   getTopElementsBounding() {
-    const topBoundings = [];
-
-    this.elements.forEach((element) => {
+    return this.elements.map((element) => {
       const topBounding = element.getBoundingClientRect().top;
 
-      topBoundings.push({
+      return {
         start: topBounding - window.innerHeight,
         end: topBounding + element.clientHeight + this.endBoundingOffset,
-      });
+      };
     });
-
-    return topBoundings;
   }
   getRelativeScrollInputs() {
-    const relativeScrollInputs = [];
-
-    this.elements.forEach((element, i) => {
-      relativeScrollInputs.push(
-        new ScrollInput(this.scroller, this.topBoundings[i], this.callback.bind(this, element))
-      );
-    });
-
-    return relativeScrollInputs;
+    return this.elements.map((element, i) => new ScrollInput(this.topBoundings[i]));
   }
 }
 
