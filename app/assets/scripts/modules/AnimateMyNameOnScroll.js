9f54f75fdbd1dee205a3ae44636283e177cba6ef
youssri mhamdi <bavary1515@gmail.com>
2020-04-22 22:22:25 +0100

    implemented the basic functionality.

--- app/assets/scripts/modules/AnimateMyNameOnScroll.js
+++ app/assets/scripts/modules/AnimateMyNameOnScroll.js
@@ -3,10 +3,11 @@ import SetupParallaxEnviroment from './SetupParallaxEnviroment';
 
 class AnimateMyNameOnScroll extends SetupParallaxEnviroment {
   constructor() {
-    super('.greeting__my-name', { top: document.querySelector('.greeting__my-name').clientHeight, bottom: -240 });
+    super('.me__my-name', { top: document.querySelector('.me__my-name').clientHeight, bottom: -240 });
+    this.myImage = document.querySelector('.me__my-image');
     this.limits = { start: -300, end: 0 };
-    this.waypoints = [300, 250, 180, 110, 40, 0];
-    this.texts = ['yossri', 'here we go!', 'keep scrolling', 'almost', 'yossri'];
+    this.waypoints = [300, 250, 160, 50];
+    this.texts = ['hello!', "keep scrolling, don't stop", 'almost', 'yossri'];
 
     this.setTransformObjects();
     this.fixedContainerWidth();
@@ -19,11 +20,24 @@ class AnimateMyNameOnScroll extends SetupParallaxEnviroment {
   }
 
   changeText() {
+    const yValue = this.getYValue();
     this.texts.forEach((text, i) => {
-      if (this.getYValue() <= this.waypoints[i]) {
+      if (yValue <= this.waypoints[i]) {
         this.elements[0].innerText = text;
+
+        if (i === 1 || i === 3) {
+          this.elements[0].classList.add('me__my-name--stroke');
+        } else {
+          this.elements[0].classList.remove('me__my-name--stroke');
+        }
       }
     });
+
+    if (yValue === 0) {
+      this.myImage.style.opacity = 1;
+    } else {
+      this.myImage.style.opacity = 0;
+    }
   }
 
   getYValue() {
@@ -31,9 +45,10 @@ class AnimateMyNameOnScroll extends SetupParallaxEnviroment {
   }
 
   fixedContainerWidth() {
-    const container = document.querySelector('.greeting');
-
+    const container = document.querySelector('.me');
+    const wrapper = document.querySelector('.me__wrapper');
     container.style.width = `${container.clientWidth}px`;
+    wrapper.style.width = `${wrapper.clientWidth}px`;
   }
 }
 
