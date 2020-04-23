3fecb17a1bd0c3f12c845481d58fbba7c437dbef
yosri mhamdi <bavary1515@gmail.com>
2020-04-23 22:31:04 +0100

    improved animateMyNameOnScroll class code.

--- app/assets/scripts/modules/AnimateMyNameOnScroll.js
+++ app/assets/scripts/modules/AnimateMyNameOnScroll.js
@@ -3,8 +3,16 @@ import SetupParallaxEnviroment from './SetupParallaxEnviroment';
 
 class AnimateMyNameOnScroll extends SetupParallaxEnviroment {
   constructor() {
-    super('.me__my-name', { top: document.querySelector('.me__my-name').clientHeight / 2, bottom: -240 });
-    this.myImage = document.querySelector('.me__my-image');
+    super('.me__my-name', {
+      top: document.querySelector('.me__my-name').clientHeight / 2,
+      bottom: -240,
+    });
+    this.DOM = {
+      myName: this.elements[0],
+      myImage: document.querySelector('.me__my-image'),
+      container: document.querySelector('.me'),
+      wrapper: document.querySelector('.me__wrapper'),
+    };
     this.limits = { start: -300, end: 0 };
     this.waypoints = [300, 250, 160, 65];
     this.texts = ['hello!', "keep scrolling, don't stop", 'almost', 'yossri'];
@@ -12,46 +20,49 @@ class AnimateMyNameOnScroll extends SetupParallaxEnviroment {
     this.setTransformObjects();
     this.fixedContainersWidth();
 
-    scroller.addListener(this.changeText.bind(this));
+    scroller.addListener(this.animate.bind(this));
   }
 
   transformFunction() {
     this.element.style.transform = `translateY(${this.getCurrentValue()}px)`;
   }
 
-  changeText() {
+  animate() {
     const yValue = this.getYValue();
 
     this.texts.forEach((text, i) => {
       if (yValue <= this.waypoints[i]) {
-        this.elements[0].innerText = text;
-
-        if (i === 1) {
-          this.elements[0].classList.add('me__my-name--stroke');
-        } else {
-          this.elements[0].classList.remove('me__my-name--stroke');
-        }
+        this.DOM.myName.innerText = text;
+        this.toggleStrokeClass(i, yValue);
       }
     });
 
-    if (yValue === 0) {
-      this.elements[0].classList.add('me__my-name--stroke');
-      this.myImage.classList.add('me__my-image--is-visible');
+    this.showImage(yValue);
+  }
+
+  toggleStrokeClass(i, yValue) {
+    if (i === 1 || yValue === 0) {
+      this.DOM.myName.classList.add('me__my-name--stroke');
     } else {
-      this.myImage.classList.remove('me__my-image--is-visible');
+      this.DOM.myName.classList.remove('me__my-name--stroke');
+    }
+  }
+
+  showImage(yValue) {
+    if (yValue === 0) {
+      this.DOM.myImage.classList.add('me__my-image--is-visible');
+    } else {
+      this.DOM.myImage.classList.remove('me__my-image--is-visible');
     }
   }
 
   getYValue() {
-    return Math.abs(parseInt(this.elements[0].style.transform.slice(11, -3)));
+    return Math.abs(parseInt(this.DOM.myName.style.transform.slice(11, -3)));
   }
 
   fixedContainersWidth() {
-    const container = document.querySelector('.me');
-    const wrapper = document.querySelector('.me__wrapper');
-
-    container.style.width = `${container.clientWidth}px`;
-    wrapper.style.width = `${wrapper.clientWidth}px`;
+    this.DOM.container.style.width = `${this.DOM.container.clientWidth}px`;
+    this.DOM.wrapper.style.width = `${this.DOM.wrapper.clientWidth}px`;
   }
 }
 
