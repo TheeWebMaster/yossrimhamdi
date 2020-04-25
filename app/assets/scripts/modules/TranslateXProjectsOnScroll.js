5a0ec403cc9ad493cca5af87fb36102141b8e30e
yosri mhamdi <bavary1515@gmail.com>
2020-04-25 13:20:12 +0100

    adjusted project parallax limits on different screen sizes.

--- app/assets/scripts/modules/TranslateXProjectsOnScroll.js
+++ app/assets/scripts/modules/TranslateXProjectsOnScroll.js
@@ -4,13 +4,25 @@ import SetupParallaxEnviroment from './SetupParallaxEnviroment';
 class TranslateXProjectsOnScroll extends SetupParallaxEnviroment {
   constructor() {
     super('.project');
-    this.limits = [
-      { start: 100, end: 400 },
-      { start: 100, end: -400 },
-    ];
+    this.limits = this.getLimits();
 
     this.animate();
   }
+
+  getLimits() {
+    if (window.innerWidth > 1400) {
+      return [
+        { start: 100, end: 400 },
+        { start: 100, end: -200 },
+      ];
+    } else {
+      return [
+        { start: 100, end: 280 },
+        { start: 100, end: -80 },
+      ];
+    }
+  }
+
   animate() {
     this.elements.forEach((element, i) => {
       const relativeLimit = i % 2 === 0 ? this.limits[0] : this.limits[1];
