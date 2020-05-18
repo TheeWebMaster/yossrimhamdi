b058ce03a1854685e1932b71f85446a2766dad4a
yossri mhamdi <bavary1515@gmail.com>
2020-05-18 17:50:40 +0100

    [fix] headline parallax jumping.

--- app/assets/scripts/modules/TranslateXFrontalHeadlinesOnScroll.js
+++ app/assets/scripts/modules/TranslateXFrontalHeadlinesOnScroll.js
@@ -3,16 +3,24 @@ import Parallax from './Parallax';
 class TranslateXFrontalHeadlinesOnScroll {
   constructor() {
     this.headlines = document.querySelectorAll('.frontal-headline--to-translate');
-    this.transLimits = [
-      { start: 0, end: -200 },
-      { start: 0, end: 200 },
-    ];
+    this.limits = {
+      transform: [
+        { start: 0, end: -200 },
+        { start: 0, end: 200 },
+      ],
+      bounding: [
+        { top: window.innerHeight - this.headlines[0].getBoundingClientRect().top, bottom: 0 },
+        { top: window.innerHeight - this.headlines[1].getBoundingClientRect().top, bottom: 0 },
+      ],
+    };
 
     this.setupParallaxObject();
   }
 
   setupParallaxObject() {
-    this.headlines.forEach((headline, i) => new Parallax(headline, this.transLimits[i]));
+    this.headlines.forEach((headline, i) => {
+      new Parallax(headline, this.limits.transform[i], this.limits.bounding[i]);
+    });
   }
 }
 
