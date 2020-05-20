79ab770a09be255a59a4fd75a51f8f14b98e63aa
yossri mhamdi <bavary1515@gmail.com>
2020-05-20 16:17:30 +0100

    changed  css class name.

--- app/assets/scripts/modules/LinkHoverAnimation.js
+++ app/assets/scripts/modules/LinkHoverAnimation.js
@@ -9,21 +9,21 @@ class LinkHoverAnimation {
   constructLink() {
     const word = this.link.textContent;
 
-    const constructedLink = `<span class="link-animation__lower">${this.getDividedLink(
+    const constructedLink = `<span class="link-hover-animation__lower">${this.getDividedLink(
       word
-    )}</span><span class="link-animation__capital">${this.getDividedLink(word)}</span>`;
+    )}</span><span class="link-hover-animation__capital">${this.getDividedLink(word)}</span>`;
 
     this.link.innerHTML = constructedLink;
   }
 
   addAnimationClass() {
-    this.link.classList.add('link-animation');
+    this.link.classList.add('link-hover-animation');
   }
 
   getDividedLink(word) {
     const letters = word.split('');
     const l = letters.map(letter => {
-      return `<span class="link-animation__letter-wrapper"><span class="link-animation__letter">${letter}</span></span>`;
+      return `<span class="link-hover-animation__letter-wrapper"><span class="link-hover-animation__letter">${letter}</span></span>`;
     });
 
     return l.join('');
