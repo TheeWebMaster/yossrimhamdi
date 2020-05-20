7181125ad04b3f448fdb60a5195b43e1811b9c51
yossri mhamdi <bavary1515@gmail.com>
2020-05-20 08:18:59 +0100

    added link on project.

--- app/assets/scripts/modules/GenerateProjects.js
+++ app/assets/scripts/modules/GenerateProjects.js
@@ -3,45 +3,56 @@ class GenerateProjects {
     this.projectsContainer = document.querySelector('.projects-container');
     this.projects = {
       title: ['roots', 'mackmedia', 'ammolite', 'grape garage', 'dorfjungs', 'mueller', 'my portfolio'],
-      marqee: 'javascript animations smooth wordpress tansition parallax react nextjs scrolling smoovy gulp post-css webpack node-js automation github git npm'.split(
-        ' '
-      ),
+      link: ['#', '#', '#', '#', '#', '#', 'https://yossrimhamdi.netlify.com'],
+      marqee:
+        'javascript animations smooth wordpress tansition parallax react nextjs scrolling smoovy gulp post-css webpack node-js automation github git npm',
     };
     this.generateDOMprojects();
   }
+
   generateDOMprojects() {
-    this.projects.title.forEach((projectTitle) => {
-      this.projectsContainer.insertAdjacentHTML('beforeend', this.getProjectDOMString(projectTitle));
+    this.projects.title.forEach((projectTitle, i) => {
+      this.projectsContainer.insertAdjacentHTML('beforeend', this.getProjectDOMString(projectTitle, this.projects.link[i]));
     });
   }
-  getProjectDOMString(projectTitle) {
+
+  getProjectDOMString(projectTitle, projectLink) {
     this.shuffleMarqee();
+
     return `
     <div class="project">
       <div class="project__container"> 
         <div class="project__wrapper">
           <h1 class="project__title">${projectTitle}</h1>
-          <p class="project__marqee">${this.projects.marqee.join(' ')}</p>
-          <p class="project__marqee project__marqee--left-content-support">${this.projects.marqee.join(' ')}</p>
+          <p class="project__marqee">${this.projects.marqee}</p>
+          <p class="project__marqee project__marqee--left-content-support">${this.projects.marqee}</p>
         </div>
-        <div class="project__image-container" data-project-title="${projectTitle.split(' ').join('-')}"></div>
+        <a href="${projectLink}" target="_blank">
+          <div class="project__image-container" data-project-title="${projectTitle.split(' ').join('-')}"></div>
+        <a/>
       </div>
     </div>
     `;
   }
+
   shuffleMarqee() {
-    for (let i = 0; i < this.projects.marqee.length; i++) {
-      this.swap(i, this.getRandomI());
+    const marqeeArray = this.projects.marqee.split(' ');
+
+    for (let i = 0; i < marqeeArray.length; i++) {
+      this.swap(marqeeArray, i, this.getRandomI(marqeeArray.length));
     }
+
+    this.projects.marqee = marqeeArray.join(' ');
   }
-  getRandomI() {
-    return Math.floor(Math.random() * this.projects.marqee.length);
+
+  getRandomI(arrayLength) {
+    return Math.floor(Math.random() * arrayLength);
   }
-  swap(i, j) {
-    const { marqee } = this.projects;
-    const temp = marqee[i];
-    marqee[i] = marqee[j];
-    marqee[j] = temp;
+
+  swap(marqeeArray, i, j) {
+    const temp = marqeeArray[i];
+    marqeeArray[i] = marqeeArray[j];
+    marqeeArray[j] = temp;
   }
 }
 
